4cadafad0e03a20cdc988f340c5ba3aed73867b1
Artur Nilson <arturnilson@gmail.com>
2021-02-16 10:04:44 -0300

    Ajuste posição dos corredores por tipo prova e por categoria

--- App/Services/ResultadosService.php
+++ App/Services/ResultadosService.php
@@ -17,7 +17,15 @@ class ResultadosService
 
     public function post()
     {
+        $this->validaPost($_POST);
         return Resultados::insert($_POST);
+    }
+
+    private function validaPost(array $post)
+    {
+        if (!$post['id_corredor'] || !$post['id_prova'] || !$post['horario_inicio'] || !$post['horario_fim']) {
+            throw new \Exception("Todos os campos devem ser preenchidos.");
+        }
     }
 
     private function prepareClassificacaoPorTipoProva(array $dados_classificacao_geral)
@@ -30,6 +38,8 @@ class ResultadosService
 
         foreach ($dados_classificacao_geral as $dados) {
             $classificacao_por_tipo_prova['tipo_prova_' . $dados['tipo_prova']][] = $dados;
+            $index = array_key_last($classificacao_por_tipo_prova['tipo_prova_' . $dados['tipo_prova']]);
+            $classificacao_por_tipo_prova['tipo_prova_' . $dados['tipo_prova']][$index]['posicao'] = $index + 1;
         }
 
         return $classificacao_por_tipo_prova;
@@ -47,18 +57,28 @@ class ResultadosService
             switch ($dados['categoria_idade']) {
                 case 1:
                     $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['18-25 anos'][] = $dados;
+                    $index = array_key_last($classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['18-25 anos']);
+                    $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['18-25 anos'][$index]['posicao'] = $index + 1;
                     break;
                 case 2:
                     $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['26-35 anos'][] = $dados;
+                    $index = array_key_last($classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['26-35 anos']);
+                    $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['26-35 anos'][$index]['posicao'] = $index + 1;
                     break;
                 case 3:
                     $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['36-45 anos'][] = $dados;
+                    $index = array_key_last($classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['36-45 anos']);
+                    $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['36-45 anos'][$index]['posicao'] = $index + 1;
                     break;
                 case 4:
                     $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['46-55 anos'][] = $dados;
+                    $index = array_key_last($classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['46-55 anos']);
+                    $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['46-55 anos'][$index]['posicao'] = $index + 1;
                     break;
                 case 5:
                     $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['55+ anos'][] = $dados;
+                    $index = array_key_last($classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['55+ anos']);
+                    $classificacao_por_grupo['tipo_prova_' . $dados['tipo_prova']]['55+ anos'][$index]['posicao'] = $index + 1;
                     break;
             }
         }
