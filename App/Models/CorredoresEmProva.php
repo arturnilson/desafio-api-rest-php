c3d22f03d2d6e616443cf9f4a1bd982a0afb2215
Artur Nilson <arturnilson@gmail.com>
2021-02-17 09:41:00 -0300

    Remoção comentário

--- App/Models/CorredoresEmProva.php
+++ App/Models/CorredoresEmProva.php
@@ -25,11 +25,6 @@ class CorredoresEmProva
     public static function insert($data)
     {
         $connectionPdo = new \PDO(DBDRIVE . ': host=' . DBHOST . '; dbname=' . DBNAME, DBUSER, DBPASS);
-        // $connectionPdo = new \PDO(DBDRIVE . ': host=' . DBHOST . '; dbname=' . DBNAME, DBUSER, DBPASS, array(
-        //     \PDO::ATTR_EMULATE_PREPARES => false,
-        //     \PDO::MYSQL_ATTR_DIRECT_QUERY => false,
-        //     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
-        // ));
 
         $sql = 'INSERT INTO ' . self::$table . '(id_corredor, id_prova) VALUES (:corredor, :prova)';
 
